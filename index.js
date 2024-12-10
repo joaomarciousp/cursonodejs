1c6fe003ffb1eb5e62c580c062d5064a05ad1117
joaomarcio <joaomarciogodoy@usp.br>
2024-12-10 14:42:26 -0300

    formulario post

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //USAR O EJS
 app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.use(express.static("public"));
 
+//CONFIGURANDO PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
-
+//ROTAS
 app.get("/", (req, res) => {
-res.render('index')
+  res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
-    res.render('perguntar')
-    });
-    
+  res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  const pergunta = req.body;
+
+  res.send(`Título: ${pergunta.titulo} e descrição: ${pergunta.descricao}`);
+});
 
 //ABRINDO SERVIDOR
 app.listen(3000, () => {
